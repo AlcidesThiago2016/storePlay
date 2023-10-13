844954932828b702f7ecba5c9f91092fed915b5d
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2023-10-13 15:43:38 -0300

    UPDATE PROJECT

--- src/app/components/card/card-price/card-price.component.ts
+++ src/app/components/card/card-price/card-price.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-price',
   templateUrl: './card-price.component.html',
   styleUrls: ['./card-price.component.css']
 })
-export class CardPriceComponent {
+export class CardPriceComponent implements OnInit{
+
+  @Input()
+  gameType:string="Digital PS4"
+  @Input()
+  gamePrice:string="R$ 399,00"
+  constructor(){}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
